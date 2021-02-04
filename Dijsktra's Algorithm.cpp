d16f239b413a1654e7bc667fec17bb82df8b202e
Pavankumardontha <pavankumardontha@gmail.com>
2021-02-04 13:36:09 +0530

    Update Dijsktra's Algorithm.cpp

--- Dijsktra's Algorithm.cpp
+++ Dijsktra's Algorithm.cpp
@@ -8,8 +8,8 @@ Finding min/max in Priority Queue O(1).
 #include<iostream>
 using namespace std;
 //we will create adjacency list but this time we use pair because we also have weights now 
-vector<pair<int,int> > g[10001];//this is adjacencylist for a weight graph.So we have two techniques one is the struct edge 
-//and the other is this
+vector<pair<int,int> > g[10001];//this is adjacencylist for a weight graph.So we have two techniques one is the struct edge which we use in krushkals algo and the other is 
+// this.Note also that the first member in this pair, is the weight and NOT the distance.!!!!! Distance is only stored in the distance array and in the priority queue.
 
 //we use distance array which stores  the sum of weights of all the edges in the shortest path from out source node to the
 //current node.
@@ -17,9 +17,9 @@ vector<pair<int,int> > g[10001];//this is adjacencylist for a weight graph.So we
 int dis[10001];
 
 //we also use Priority Queue in this method
-priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > > pq;
+priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > > pq; //this is min heap implementation.Min heap with respect to the first element in the pair.
 //priority_queue<pair<int,int>> pq; //this is normal max heap implementation
-
+//the first element in the above pair in the priority queue is the distance and NOT the weight.
 
 
 int main()
@@ -43,7 +43,7 @@ int main()
 	//we push the src node into the priority queue and do it same as the BFS
 	
 	cin>>src;
-	pq.push(make_pair(0,src));
+	pq.push(make_pair(0,src)); //Note that here we are pushing distance as first element of the pair and node as the second element of the pair.
 	dis[src]=0;
 	/* make sure that in the graph vector we have weights and the children but in the priority queue we will have the distance
 	of the nodes from the source node.*/
@@ -56,10 +56,12 @@ source and then only push that node with its distance into the priority queue.*/
 		int d=temp.first;
 		int node=temp.second;
 		pq.pop();
+/*we now have our current node and the distance of it from the source node. We will add this distance and weight associated with different edges and find the distance of its
+children. We will update and push into the priority queue if needed.*/
 		for(int i=0;i<g[node].size();i++)
 		{
-			pair<int,int> edge =g[node][i];
-			//we have to think whether we should add this edge into our priority list or not.
+			pair<int,int> edge =g[node][i]; /* this is an edge between our current node and its children.In other words we are traversing the adjacency list of 
+our current node.We will push this into the priority queue if and only if we update its distance.*/
 			if(d+edge.first<dis[edge.second])
 			{
 				dis[edge.second]=d+edge.first;
