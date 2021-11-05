4732f09fce7d3dc9bc844f834f9e532ffb823d2e
Pavankumardontha <pavankumardontha@gmail.com>
2021-11-05 18:18:18 +0530

    Update and rename Floyd Warshall Algorithm to Floyd_warshall.cpp

---
+++ Floyd_warshall.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int n,m;
+int dis[101][101];
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i==j)
+            dis[i][j]=0;
+            else
+            dis[i][j]=INT_MAX;
+        }
+    }
+    int a,b,w;
+    for(int i=0;i<m;i++)
+    {
+        cin>>a>>b>>w;
+        dis[a][b]=w;
+    }
+    1 node nunchi every node distance 
+    1{
+        1,2,3,4,5,6,7,8,9 
+        {
+            1,1, ====> 1,2,3,4,5,6,7,8,9,
+            1,2  ====> 1,2,3,4,5,6,7,8,9,
+            dis[1][2] = dis[1][j] + dis[j][2]
+            
+        }
+    }
+    for(int k=1;k<=n;k++) //src 
+    { 
+        for(int i=1;i<=n;i++)  //final 
+        {
+            //our aim is to calculate minimum distance between (k,i)
+            for(int j=1;j<=n;j++) //middle man 
+            {
+                if(dis[k][j]!=INT_MAX && dis[j][i]!=INT_MAX) //route present or not  
+                if(dis[k][i] > dis[k][j] + dis[j][i]) //compare the distance 
+                dis[k][i] = dis[k][j] + dis[j][i];   //update 
+            }
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        cout<<dis[i][j]<<" ";
+        cout<<endl;
+        
+    }
+}
