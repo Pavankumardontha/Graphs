3d062e9a028d063cc9603d68167dd23037d95481
Pavankumardontha <pavankumardontha@gmail.com>
2022-08-13 03:03:48 +0530

    Update Word_ladder_1.cpp

--- Word_ladder_1.cpp
+++ Word_ladder_1.cpp
@@ -1,95 +1,38 @@
 // link :- https://leetcode.com/problems/word-ladder/
 
-vector<int> g[5001];
-bool differbyone(string s1, string s2)
-{
-    int count = 0;
-    if(s1.length()!=s2.length())
-        return false;
-    else
-    {
-        int n = s1.length();
-        int c = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(s1[i]!=s2[i])
-                c=c+1;
-        }
-        if(c==1)
-            return true;
-        else
-            return false;
-    }
-}
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
     {
-        //check if endWord is present or not
-        int endword_index = -1;
-        int beginword_index = -1;
+        queue<pair<int,string>> q;
+        unordered_set<string> words;
         for(int i=0;i<wordList.size();i++)
-        {
-            if(endWord==wordList[i])
-                endword_index = i;
-            if(beginWord==wordList[i])
-                beginword_index = i;
-        }
-        if(endword_index==-1)
+            words.insert(wordList[i]);
+        if(words.find(endWord)==words.end())
             return 0;
-        
-        // push the beginword into the wordlist if it is not present in the wordList
-        if(beginword_index==-1)
+        q.push({1,beginWord});
+        while(!q.empty())
         {
-            wordList.push_back(beginWord);
-            beginword_index = wordList.size()-1;
-        }
-        int n = wordList.size();
-        for(int i=0;i<n;i++)
-            g[i].clear();
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
+            pair<int,string> temp = q.front();
+            string s = temp.second;
+            int distance = temp.first;
+            q.pop();
+            for(char c='a';c<='z';c++)
             {
-                // if both the strings differ by one character then there exists an edge between them
-                bool check_diff = differbyone(wordList[i],wordList[j]);
-                if(check_diff)
+                for(int i=0;i<s.length();i++)
                 {
-                    g[i].push_back(j);
-                    g[j].push_back(i);
+                    string original = s;
+                    original[i] = c;
+                    if(original==endWord)
+                        return distance + 1;
+                    if(words.find(original)!=words.end())
+                    {
+                        words.erase(original);
+                        q.push({distance+1,original});
+                    }
                 }
             }
         }
-        int dis[n];
-        for(int i=0;i<n;i++)
-            dis[i]=INT_MAX;
-        bool vis[n];
-        for(int i=0;i<n;i++)
-            vis[i]=false;
-        dis[beginword_index]=0;
-        queue<int> q;
-        q.push(beginword_index);
-        vis[beginword_index]=true;
-        while(!q.empty())
-        {
-            int v = q.front();
-            q.pop();
-            for(int i=0;i<g[v].size();i++)
-            {
-                int child = g[v][i];
-                if(!vis[child])
-                {
-                    dis[child] = dis[v] + 1;
-                    q.push(child);
-                    vis[child]=true;
-                }
-            } 
-        }
-        if(dis[endword_index]==INT_MAX)
-            return 0;
-        else
-            return dis[endword_index]+1;
-        
+        return 0;
     }
 };
