a699f2f7a270dbca25a7b963920590a62ceeb15b
Pavankumardontha <pavankumardontha@gmail.com>
2022-10-16 16:20:21 +0530

    Update Diameter_of_a_Tree(using_BFS).cpp

--- Diameter_of_a_Tree(using_BFS).cpp
+++ Diameter_of_a_Tree(using_BFS).cpp
@@ -21,55 +21,65 @@ Output:
 
 
 #include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-vector<int> g[10001];//this is adjacency list
-int vis[10001];
-int maxD;//maximum distance
-int maxNode;//node which is at the maximum distance from the current node from which we have started the dfs
-void dfs(int v,int d);//v=current node,d=its distance from where we have started the dfs
+
+vector<int> g[10001];
+int n;
+int m;
+int farest_node;
+int longest_distance = 0; // longest distance between 2 nodes
+int dis[10001];
 int main()
 {
-	int n,a,b;
-   cin>>n;
-   for(int i=1;i<n;i++)
-   {
-   	cin>>a>>b;
-   	g[a].push_back(b);
-   	g[b].push_back(a);
-   	
-   }
-   //adjacency list is ready 
-   //to find diameter of a tree we have to apply dfs 2 times
-   //we have to now initialise our maxD to -1 before applying the dfs.
-   
-   
-   //first dfs call.
-   maxD=-1;
-   dfs(1,0);
-   //before second dfs call we have to initialse the vis array zero again 
-   for(int i=1;i<=n;i++)
-   vis[i]=0;
-   //now the second call has to be called starting from the maxNode obtained from first dfs call.
-   dfs(maxNode,0);
-   //now we have the diameter stored in the maxD variable 
-   cout<<maxD<<endl;
-   
-   
-}
-
-void dfs(int v,int d)
-{
-	vis[v]=1;
-	if(d>maxD)
-	{
-		maxD=d;
-		maxNode=v;
-	}
-	for(int i=0;i<g[v].size();i++)
-	{
-		int child=g[v][i];
-		if(vis[child]==0)
-		dfs(child,d+1);
-	}
-	
+    cin>>n;
+    m=n-1;
+    int a,b;
+    while(m--)
+    {
+        cin>>a>>b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    for(int i=1;i<=n;i++)
+    dis[i]=INT_MAX;
+    queue<int> q;
+    q.push(1);
+    dis[1]=0;
+    while(!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        for(int i=0;i<g[v].size();i++)
+        {
+            int child = g[v][i];
+            if(dis[child]==INT_MAX)
+            {
+                dis[child] = dis[v]+1;
+                q.push(child);
+            }
+        }
+        farest_node = v;
+    }
+    // start bfs from the farest_node 
+    for(int i=0;i<=n;i++)
+    dis[i]=INT_MAX;
+    q.push(farest_node);
+    dis[farest_node]=0;
+    while(!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        for(int i=0;i<g[v].size();i++)
+        {
+            int child = g[v][i];
+            if(dis[child]==INT_MAX)
+            {
+                dis[child] = dis[v]+1;
+                q.push(child);
+            }
+        }
+        longest_distance = dis[v];
+    }
+    cout<<longest_distance<<endl;
 }
